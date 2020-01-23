c692d4e1d2fededf5a00258518f1ae755534da82
Kelaric <43745122+Kelaric@users.noreply.github.com>
2020-01-23 14:51:28 +0100

    Java API Crypto Library

--- src/main/java/Adapter.java
+++ src/main/java/Adapter.java
@@ -1,13 +1,7 @@
-import org.json.simple.parser.JSONParser;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+
 
 public class Adapter {
     public static void main(String[] args) throws IOException, ParseException {
